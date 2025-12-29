0f26f5f3ddcaa6ec6eba8784734f2da77c5e472e
Liza <lizamezioug22@gmail.com>
2025-12-29 18:26:31 +0100

    Ajout CV, image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -271,7 +271,7 @@ const HeroSection = ({ isDark }) => {
           </button>
 
           <a
-            href="/src/assets/CV EN.pdf"
+            href="/public/CV EN.pdf"
             download="MEZIOUG_Liza_CV.pdf"
             className={`group px-8 py-3 rounded-full transition-all duration-300 border-2 ${
               isDark 
